d9fbbeacf47c9b50df92a6f74af36fa84e49dd3d
Kimutai01 <kiprotichkimutai01@gmail.com>
2025-02-12 09:09:51 +0300

    initialization and first widgets

---
+++ lib/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(child: StyledText()),
+    );
+  }
+}
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+
+  @override
+  Widget build(context) {
+    return Text(
+      "Text from widget",
+      style: TextStyle(color: Colors.white, fontSize: 28),
+    );
+  }
+}
